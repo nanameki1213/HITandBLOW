1f2a33a870dfaeb630df51d8c05df51049d98fea
nanameki1213 <101384410+nanameki1213@users.noreply.github.com>
2023-06-27 17:07:51 +0900

    プロトタイプのファイルを追加

---
+++ prototype.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define DIGITS_NUM 3
+
+int main(void) {
+  int ans[DIGITS_NUM];
+  int i, j;
+  int hit_num = 0, blow_num = 0;
+
+  srand((unsigned)time(NULL));
+
+  // 1. 答えとなる数を設定する
+
+  while(/* プレイヤーが正解しない間 */) {
+    int input[DIGITS_NUM];
+    int hit_num = 0, blow_num = 0;
+
+    // 2. プレイヤーからの数の入力を行う
+
+    // 3. 入力された数のヒット数を計算
+
+    // 4. 入力された数のブロー数を計算
+
+    // 5. 結果を表示
+
+  }
+
+  return 0;
+}
